6d9ccbc6d6b847c818e51033211f6ead46392c9f
Kiehyun Kevin Park <guitar79@naver.com>
2019-05-20 16:40:40 +0900

    Add files via upload

--- convert_cr2_jpg_CLAHE.py
+++ convert_cr2_jpg_CLAHE.py
@@ -21,7 +21,12 @@ debuging = False
 add_log = True
 if add_log == True :
     log_file = 'python_log.txt'
-
+    
+def write_log(log_file, log_str):
+    with open(log_file, 'a') as log_f:
+        log_f.write(log_str+'\n')
+    return print (log_str)
+        
 #for checking time
 cht_start_time = datetime.now()
 def print_working_time():
@@ -45,30 +50,39 @@ def clahe_equalized(imgs):
     return bgr
 save_dir_name = 'jpeg_CLAHE/'
 base_dir_name = '../190504.WooUm_Island.5Dm2/'
-base_dir_names = ['../190504.WooUm_Island.5Dm2/', '../190505.JC_fossil.5Dm2/']
+base_dir_names = ['../190504.WooUm_Island.5Dm2/', '../190505.JC_fossil.5Dm2']
+
+base_dir_names = []
+list_dir = os.listdir('../')
+for i in list_dir : 
+    base_dir_names.append('../'+i+'/')
 
 for base_dir_name in base_dir_names :
 
     if not os.path.exists(base_dir_name+save_dir_name):
         os.makedirs(base_dir_name+save_dir_name)
-        print ('{0}{1} is created'.format(base_dir_name, save_dir_name))
+        write_log(log_file, '{0} ::: {1}{2} is created'.format(datetime.now(), base_dir_name, save_dir_name))
     else : 
-        print ('{0}{1} is already exist'.format(base_dir_name, save_dir_name))
+        write_log(log_file, '{0} ::: {1}{2} is already exist'.format(datetime.now(), base_dir_name, save_dir_name))
     
     ### Start process
     img_lists = sorted(glob(os.path.join(base_dir_name, '*.cr2')))
     
     for raw_name in img_lists:
-        with rawpy.imread(raw_name) as raw:
-            img = raw.postprocess()
-        img = clahe_equalized(img)
-        img = bgr2rgb(img)
-        
-        #save aligned image
-        if debuging == True : 
-            print("debugging::: # Save final results")
-        print('{0}{1}{2}.jpg'.format(base_dir_name, save_dir_name, raw_name[-25:-4]))
-        cv2.imwrite('{0}{1}{2}.jpg'.format(base_dir_name, save_dir_name, raw_name[-25:-4]), img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+        if not os.path.isfile('{0}{1}{2}.jpg'.format(base_dir_name, save_dir_name, raw_name[-25:-4])):
+            try : 
+                with rawpy.imread(raw_name) as raw:
+                    img = raw.postprocess()
+                img = clahe_equalized(img)
+                img = bgr2rgb(img)
+                
+                # Save final results
+                write_log(log_file, '{0} ::: {1}{2}{3}.jpg is created'.format(datetime.now(), base_dir_name, save_dir_name, raw_name[-25:-4]))
+                cv2.imwrite('{0}{1}{2}.jpg'.format(base_dir_name, save_dir_name, raw_name[-25:-4]), img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            except Exception as err :
+                write_log(log_file, '{0} ::: {1} with {2}'.format(datetime.now(), err, raw_name))
+        else :
+            write_log(log_file, '{0} ::: {1}{2}{3}.jpg is already exist'.format(datetime.now(), base_dir_name, save_dir_name, raw_name[-25:-4]))
     
         print_working_time()
     print_working_time()
