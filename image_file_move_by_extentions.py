6c66ff5b062993020adde4c98115979200fbf46f
guitar79 <guitar79@naver.com>
2021-04-01 20:10:20 +0900

    good to pycharm

--- image_file_move_by_extentions.py
+++ image_file_move_by_extentions.py
@@ -23,63 +23,69 @@ import os
 from datetime import datetime
 import photo_utilities
 
-ext_name = "jpg"
+ext_names = ["dng", "jpg", "png", "heic", "cr2"]
+#ext_name = "dng"
+#ext_name = "jpg"
 #ext_name = "png"
 #ext_name = "heic"
-ext_name = "cr2"
-
-add_log = True
-if add_log == True :
-    log_file = 'iamge_file_move_by_extentions-.log'
+#ext_name = "cr2"
+#ext_name = "dng"
 
 base_dir_name = '../New_Photo/'
 
 fullnames = photo_utilities.getFullnameListOfallFiles(base_dir_name)
 
-save_base_dir_name = "../../{}file/".format(ext_name)
+for ext_name in ext_names :
 
-image_Num = 0
-for fullname in fullnames[:]:
-#fullname = fullnames[10]
-    if fullname[-4:].lower() == ".{}".format(ext_name) :
-        image_Num += 1
-        try :
-            print("Trying the file :\n{}".format(fullname))
-            image_datetime = photo_utilities.get_image_datetime_str(fullname)
-            image_ModelID = photo_utilities.get_image_Model_name(fullname).replace(' ','')
-            image_Software = photo_utilities.get_image_Software(fullname)
+    # for debugging
+    debuging = False
+    add_log = True
+    if add_log == True:
+        log_file = "{}_file_rename.log".format(ext_name)
 
-            save_dir_name = '{0}{1}/{1}-{2}-{3}_{4}_{5}/'\
-                      .format(save_base_dir_name, image_datetime[0:4], \
-                      image_datetime[4:6], image_datetime[6:8], image_ModelID, image_Software)
+    save_base_dir_name = "../../{}file/".format(ext_name)
 
-            while not os.path.exists(save_dir_name):
-                os.makedirs(save_dir_name)
-                print ("*"*80)
-                print ("{0} is created".format(save_dir_name))
-                    
-            if not os.path.exists("{0}{1}_{2}_{3}_{4:08d}_py.{5}"\
-                      .format(save_dir_name, image_datetime, \
-                      image_ModelID, image_Software, image_Num, ext_name)):
-                os.rename(fullname,\
-                      "{0}{1}_{2}_{3}_{4:08d}_py.{5}"\
-                      .format(save_dir_name, image_datetime, \
-                      image_ModelID, image_Software, image_Num, ext_name))
-                photo_utilities.write_log(log_file, \
-                      "{6} is moved to {0}{1}_{2}_{3}_{4:08d}_py.{5}"\
-                      .format(save_dir_name, image_datetime, \
-                      image_ModelID, image_Software, image_Num, ext_name, fullname))
-            else : 
-                photo_utilities.write_log(log_file, \
-                      "{6} cannot move to {0}{1}_{2}_{3}_{4:08d}_py.{5}"\
-                      .format(save_dir_name, image_datetime, \
-                      image_ModelID, image_Software, image_Num, ext_name, fullname))
+    image_Num = 0
+    for fullname in fullnames[:]:
+    #fullname = fullnames[10]
+        if fullname[-4:].lower() == ".{}".format(ext_name) :
+            image_Num += 1
+            try :
+                print("Trying the file :\n{}".format(fullname))
+                image_datetime = photo_utilities.get_image_datetime_str(fullname)
+                image_ModelID = photo_utilities.get_image_Model_name(fullname).replace(' ','')
+                image_Software = photo_utilities.get_image_Software(fullname)
 
-        except Exception as err :
-            photo_utilities.write_log(log_file, 
-              "{2} ::: {0} error with {1}".format(err, fullname, datetime.now()))
+                save_dir_name = '{0}{1}/{1}-{2}-{3}_{4}_{5}/'\
+                          .format(save_base_dir_name, image_datetime[0:4], \
+                          image_datetime[4:6], image_datetime[6:8], image_ModelID, image_Software)
 
-        
+                while not os.path.exists(save_dir_name):
+                    os.makedirs(save_dir_name)
+                    print ("*"*80)
+                    print ("{0} is created".format(save_dir_name))
+
+                if not os.path.exists("{0}{1}_{2}_{3}_{4:08d}_py.{5}"\
+                          .format(save_dir_name, image_datetime, \
+                          image_ModelID, image_Software, image_Num, ext_name)):
+                    os.rename(fullname,\
+                          "{0}{1}_{2}_{3}_{4:08d}_py.{5}"\
+                          .format(save_dir_name, image_datetime, \
+                          image_ModelID, image_Software, image_Num, ext_name))
+                    photo_utilities.write_log(log_file, \
+                          "{6} is moved to {0}{1}_{2}_{3}_{4:08d}_py.{5}"\
+                          .format(save_dir_name, image_datetime, \
+                          image_ModelID, image_Software, image_Num, ext_name, fullname))
+                else :
+                    photo_utilities.write_log(log_file, \
+                          "{6} cannot move to {0}{1}_{2}_{3}_{4:08d}_py.{5}"\
+                          .format(save_dir_name, image_datetime, \
+                          image_ModelID, image_Software, image_Num, ext_name, fullname))
+
+            except Exception as err :
+                photo_utilities.write_log(log_file,
+                  "{2} ::: {0} error with {1}".format(err, fullname, datetime.now()))
+
 #############################################################################
 #############################################################################
 #############################################################################
--- movie_file_move_by_extentions.py
+++ movie_file_move_by_extentions.py
@@ -23,52 +23,56 @@ import os
 from datetime import datetime
 import photo_utilities
 
+
+ext_names = ["mp4", "mov", "3gp"]
 ext_name = "mp4"
 #ext_name = "mov"
 #ext_name = "3gp"
-
-#for debugging
-debuging = False
-add_log = True
-if add_log == True :
-    log_file = "{}_file_rename.log".format(ext_name)
 
 base_dir_name = "../New_Photo/"
 
 fullnames = photo_utilities.getFullnameListOfallFiles(base_dir_name)
 
-save_base_dir_name = "../../{}file/".format(ext_name)
+for ext_name in ext_names :
 
-Img_N = 0
+    # for debugging
+    debuging = False
+    add_log = True
+    if add_log == True:
+        log_file = "{}_file_rename.log".format(ext_name)
 
-for fullname in fullnames[:]:
-#fullname = fullnames[0]
-    if fullname[-4:].lower() == ".{}".format(ext_name) :
-        mov_datetime = photo_utilities.get_mov_creation_date(fullname)
-                
-        try :
-            print("Trying with {}...".format(fullname))
-            
-            save_dir_name = "{0}{1}/{1}-{2}-{3}/"\
-                  .format(save_base_dir_name, mov_datetime[0:4], 
-                          mov_datetime[4:6], mov_datetime[6:8])
-            
-            while not os.path.exists(save_dir_name):
-                os.makedirs(save_dir_name)
-                print ('*'*80)
-                print ('{0} is created'.format(save_dir_name))
-                    
-            os.rename(fullname, '{0}{1}_{2:08d}_py.{3}'\
-                      .format(save_dir_name, mov_datetime, Img_N, ext_name))
-            #print(fullname, '{0}{1}_{2:08d}_py.jpg'.format(save_dir_name, image_datetime, Img_N))
-            photo_utilities.write_log(log_file, \
-                      '{4}:::{0}{1}_{2:08d}_py.{3}'.format(save_dir_name, \
-                       mov_datetime, Img_N, ext_name, datetime.now()))
-        
-        except Exception as err :
-            photo_utilities.write_log(log_file, 
-                      '{2} ::: error {0} with {1}'.format(err, Img_N, datetime.now()))
-        Img_N += 1
+    save_base_dir_name = "../../{}file/".format(ext_name)
+
+    Img_N = 0
+
+    for fullname in fullnames[:]:
+    #fullname = fullnames[0]
+        if fullname[-4:].lower() == ".{}".format(ext_name) :
+            mov_datetime = photo_utilities.get_mov_creation_date(fullname)
+
+            try :
+                print("Trying with {}...".format(fullname))
+
+                save_dir_name = "{0}{1}/{1}-{2}-{3}/"\
+                      .format(save_base_dir_name, mov_datetime[0:4],
+                              mov_datetime[4:6], mov_datetime[6:8])
+
+                while not os.path.exists(save_dir_name):
+                    os.makedirs(save_dir_name)
+                    print ('*'*80)
+                    print ('{0} is created'.format(save_dir_name))
+
+                os.rename(fullname, '{0}{1}_{2:08d}_py.{3}'\
+                          .format(save_dir_name, mov_datetime, Img_N, ext_name))
+                #print(fullname, '{0}{1}_{2:08d}_py.jpg'.format(save_dir_name, image_datetime, Img_N))
+                photo_utilities.write_log(log_file, \
+                          '{4}:::{0}{1}_{2:08d}_py.{3}'.format(save_dir_name, \
+                           mov_datetime, Img_N, ext_name, datetime.now()))
+
+            except Exception as err :
+                photo_utilities.write_log(log_file,
+                          '{2} ::: error {0} with {1}'.format(err, Img_N, datetime.now()))
+            Img_N += 1
         
 #############################################################################
 #############################################################################
@@ -79,9 +83,7 @@ print ("fullnames: {}".format(fullnames))
 import shutil 
 
 for fullname in fullnames[:] :
-
     # Check is empty..
     if len(os.listdir(fullname)) == 0 :
         shutil.rmtree(r"{}".format(fullname)) # Delete..
         print ("rmtree {}\n".format(fullname))
-                                                   
